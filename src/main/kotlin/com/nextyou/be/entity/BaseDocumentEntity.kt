714494db808538b8db0d67f362d8c5e5fb5d3d33
Heeseung Koo <nrudev@naver.com>
2025-02-08 04:21:46 +0900

    feat: 도큐먼트 공통 필드 정의

---
+++ src/main/kotlin/com/nextyou/be/entity/BaseDocumentEntity.kt
@@ -0,0 +1,25 @@
+package com.nextyou.be.entity
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.mongodb.core.mapping.Field
+import org.springframework.data.mongodb.core.mapping.FieldType
+import java.time.LocalDateTime
+
+abstract class BaseDocumentEntity {
+    @Id
+    @Field("_id", targetType = FieldType.OBJECT_ID)
+    var id: String? = null
+        private set
+
+    @Field("created_at")
+    @CreatedDate
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        private set
+
+    @Field("updated_at")
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        private set
+}
